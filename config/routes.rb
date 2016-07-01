a5f4bfb7e5536f35d1de5156574c279f9905b51e
Martin Safer <mtsafer@gmail.com>
2016-07-01 14:21:33 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root 'static_pages#home'
 
   get 'home' => "static_pages#home"
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
 
   get 'about' => 'static_pages#about'
+
+  get 'signup' => 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-	def setup
-  	@base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
   test "should get root" do
     get root_url
     assert_response :success
@@ -14,25 +10,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_url
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", full_title()
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", full_title("Help")
   end
 
   test "should get about" do
   	get about_url
   	assert_response :success
-  	assert_select "title", "About | #{@base_title}"
+  	assert_select "title", full_title("About")
   end
 
   test "should get contact" do
   	get contact_url
   	assert_response :success
-  	assert_select "title", "Contact | #{@base_title}"
+  	assert_select "title", full_title("Contact")
   end
 
 end
